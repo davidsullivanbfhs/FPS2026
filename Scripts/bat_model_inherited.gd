7ba2d9dca052915571d0d40ad7a18bc2ffa99519
davidsullivanbfhs <dsullivan@bfhsla.org>
2026-02-06 15:21:48 -0600

    animation tree mix mode to add

--- Scripts/bat_model_inherited.gd
+++ Scripts/bat_model_inherited.gd
@@ -3,5 +3,6 @@ extends Node3D
 @onready var animation_tree: AnimationTree = %AnimationTree
 
 func hurt():
+	# make sure the oneshot node mix mode is set to add
 	animation_tree.set("parameters/OneShot/request", 
 	AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @onready var bat_model: Node3D = $bat_model_inherited
 @export var health:= 5.0
+@onready var player = get_node("/root/World/Player")
 
 func take_damage(damage):
 	health -= damage
@@ -9,4 +10,7 @@ func take_damage(damage):
 	if health <= 0:
 		set_physics_process(false)
 		gravity_scale = 1.0
+		var direction = -global_position.direction_to(player.global_position)
+		var random_upward_force = Vector3.UP * randf() * 5
+		apply_central_impulse(direction.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 10.0 * random_upward_force)
 	print(health)
