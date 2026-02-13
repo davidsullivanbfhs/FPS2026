0c7222fb372381faa67d6a3cea8662da29445ae7
davidsullivanbfhs <dsullivan@bfhsla.org>
2026-02-13 17:54:05 -0600

    spawns
    some mob work, and the beginning of a spawner

--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -1,16 +1,52 @@
 extends RigidBody3D
 
+# a signal to tell the main scene when a mob has died
+signal died
+# 
+var speed = randf_range(2.0, 4.0)
 @onready var bat_model: Node3D = $bat_model_inherited
 @export var health:= 5.0
+# a reference to the player node while the game is playing
 @onready var player = get_node("/root/World/Player")
+@onready var delete_mob_timer: Timer = %DeleteMobTimer
+@onready var ko_sound: AudioStreamPlayer3D = %KOSound
+@onready var hit_sound: AudioStreamPlayer3D = %HitSound
+
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	# we wont worry about moving up and down
+	direction.y = 0.0
+	# since the mob is a physics  body, we add physics forces to move the mob
+	linear_velocity = direction * speed
+	# rotate to face the player
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
+
 
 func take_damage(damage):
+	### need to prevent the mob from continuing to take damage after they are dead
+	### we can check if health is below 0, if it is, skip the rest of the function
+	if health <= 0:
+		return
+	# otherwise do stuff
 	health -= damage
 	bat_model.hurt()
+	hit_sound.play()
 	if health <= 0:
+		ko_sound.play()
 		set_physics_process(false)
 		gravity_scale = 1.0
 		var direction = -global_position.direction_to(player.global_position)
 		var random_upward_force = Vector3.UP * randf() * 5
 		apply_central_impulse(direction.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 10.0 * random_upward_force)
-	print(health)
+		# start the timer
+		delete_mob_timer.start()
+		# if you dont want the bat to rotate like a top if touched by the player
+		# lock the mobs rotation, and then turn it back on when shot
+		lock_rotation = false
+		# the mob sends a signal when it dies
+		died.emit()
+
+
+func _on_delete_mob_timer_timeout() -> void:
+	queue_free()
---
+++ Scripts/mob_spawner.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+# allow each spawner to spawn different mobs
+# a mob scene (from filesystem) will have to be put in to this field when placed in the level
+@export var mob_to_spawn: PackedScene = null
+
+@onready var mob_timer: Timer = %MobTimer
+@onready var mob_spawn_location: Marker3D = %MobSpawnLocation
+
+# when the mob is spawned we send a signal connecting the mob died signal to the game
+signal mob_spawned(mob)
+
+
+func _on_mob_timer_timeout() -> void:
+	# spawn a mob
+	var new_mob = mob_to_spawn.instantiate()
+	add_child(new_mob)
+	new_mob.global_position = mob_spawn_location.global_position
+
+	# emit a signal connecting the mob to the game
+	mob_spawned.emit(new_mob)
