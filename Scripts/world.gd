09335e66467ea950f5e3ccde20a85ff6664796d1
davidsullivanbfhs <dsullivan@bfhsla.org>
2026-02-13 20:34:02 -0600

    no poofs

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -14,9 +14,26 @@ func increase_score():
 	label.text = "Score: " + str(player_score)
 
 
+func do_poof(mob_position):
+	const SMOKE_PUFF = preload("res://Assets/Poof/smoke_puff.tscn")
+	var poof := SMOKE_PUFF.instantiate()
+	add_child(poof)
+	poof.global_position = mob_position
+	print("ppof")
+
+
 # when the mob is born we need to connect the signal to the game level
 func _on_mob_spawner_3d_mob_spawned(mob):
-	mob.died.connect(increase_score)
+	# connect the mob died signal to the incease score function in the game script
+	#mob.died.connect(increase_score)
+	# lambda function allows you to connect a function to a signal on the fly
+	# we can move the increase score here and call the poof when the mob dies
+	mob.died.connect(func on_mob_died():
+		increase_score()
+		do_poof(mob.global_position)
+	)
+	# do the poof when the mob is spawned
+	do_poof(mob.global_position)
 
 
 func _on_kill_plane_body_entered(body: Node3D) -> void:
